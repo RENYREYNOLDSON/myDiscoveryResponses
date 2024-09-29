7083a5f2033c4a875d1fb41199aac7064c006fe3
RENYREYNOLDSON <areynoldson7ta@gmail.com>
2024-09-29 12:48:44 +0100

    Make tweaks

--- __main__.py
+++ __main__.py
@@ -17,80 +17,82 @@
 # IMPORTS
 ############################################################################################################
 from windows.splash import *
+
 ##### Open splash screen before large module imports
-if __name__=="__main__":
+if __name__ == "__main__":
     initial_theme()
     splash_screen = Splash()
     splash_screen.update()
 ##### Run the script to import modules
 from __modules__ import *
 
+
 # MAIN WINDOW CLASS
 ############################################################################################################
-class App(tk.CTkToplevel,Saving,WindowUtility,Config,Undo,Export,Requests):
-    #CONSTRUCTOR 
-    def __init__(self,master, **kwargs):
+class App(tk.CTkToplevel, Saving, WindowUtility, Config, Undo, Export, Requests):
+    # CONSTRUCTOR
+    def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
         ##### VERSION NUMBER
-        self.version="1.1.0"
+        self.version = "1.1.0"
         #####
 
         ##### CLASS ATTRIBUTES
-        self.root=root#Master is root of the program (Top level tk)
+        self.root = root  # Master is root of the program (Top level tk)
         root.call()
-        #A list of all the open client objects
-        self.clients=[]
-        #Holds all requests in the currently selected file
-        self.reqs=[]
-        #Denotes the current client object
+        # A list of all the open client objects
+        self.clients = []
+        # Holds all requests in the currently selected file
+        self.reqs = []
+        # Denotes the current client object
         self.current_client = ""
-        #Current selected file object
+        # Current selected file object
         self.current_file = ""
-        #Pointer to the currently selected request
-        self.current_req=0
-        #Request type of the current file
-        self.req_type=""
-        #Previous request type, using for managing window
-        self.prev_type=""
-        #The previous objection text
-        self.previous_objection_text=""
-        #Same container for all of the pop out windows, only one open at once!
-        self.win=None
-        #Contains a stack of all of the previous actions, maximum size can be defined in settings
-        #Default maximum size of 100
+        # Pointer to the currently selected request
+        self.current_req = 0
+        # Request type of the current file
+        self.req_type = ""
+        # Previous request type, using for managing window
+        self.prev_type = ""
+        # The previous objection text
+        self.previous_objection_text = ""
+        # Same container for all of the pop out windows, only one open at once!
+        self.win = None
+        # Contains a stack of all of the previous actions, maximum size can be defined in settings
+        # Default maximum size of 100
         self.ACTION_STACK = []
         self.REDO_ACTION_STACK = []
 
         ##### LOAD ATTRIBUTES FROM METHODS
-        #Opens the shortcuts
-        self.HOTKEYS=open_hotkeys()
-        #Opens the recent files json
-        self.RECENTS=get_recents()
-        #Repairs the config file if new items have been added
-        #Loads the config file (software settings)
+        # Opens the shortcuts
+        self.HOTKEYS = open_hotkeys()
+        # Opens the recent files json
+        self.RECENTS = get_recents()
+        # Repairs the config file if new items have been added
+        # Loads the config file (software settings)
         validate_integrity_of_config_file()
         remove_installers()
         self.set_config()
-        #Initialises the spell checker with the current language
+        # Initialises the spell checker with the current language
         self.SPELL_CHECKER = SpellChecker(self.CONFIG["spelling"]["language"])
-        #Get the list of objections
+        # Get the list of objections
         self.objections = open_objections()
 
         ##### WINDOW SETUP
-        #Set the icon after a delay (otherwise overwritten by ctk)
-        self.after(200, lambda: self.iconbitmap(os.path.join(os.path.dirname(__file__),"assets/icon.ico")))
-        #Set minimum size of window
-        self.minsize(1050,720)
-        #Set to be fullscreened
+        # Set the icon after a delay (otherwise overwritten by ctk)
+        self.after(200, lambda: self.iconbitmap(os.path.join(os.path.dirname(__file__), "assets/icon.ico")))
+        # Set minimum size of window
+        self.minsize(1050, 720)
+        # Set to be fullscreened
         self.state("zoomed")
-        #Set the title of the main window
+        # Set the title of the main window
         self.title("myDiscoveryResponses")
 
         ##### BEGIN REGULAR INTERVAL FUNCTIONS
-        #Autosave will trigger the save function at regular intervals
-        self.after(int(self.CONFIG["general"]["autosave_interval"]),self.autosave)
-        
+        # Autosave will trigger the save function at regular intervals
+        self.after(int(self.CONFIG["general"]["autosave_interval"]), self.autosave)
+
         ##### KEY BINDINGS
         self.setup_bindings()
 
@@ -99,7 +101,7 @@ class App(tk.CTkToplevel,Saving,WindowUtility,Config,Undo,Export,Requests):
 
         ##### CHECK IF FIRM DETAILS ARE DEFAULTS
         self.check_for_new_user()
-        
+
     # Create a new MAIN window, must be here
     def create_window(self):
         create_window(self.master)
@@ -108,43 +110,34 @@ class App(tk.CTkToplevel,Saving,WindowUtility,Config,Undo,Export,Requests):
 # ROOT FUNCTIONS
 ############################################################################################################
 # Create a new window with root as parent
-def create_window(root,from_file="file which will not exist on anybody's file path"):
-    if os.path.exists(from_file.replace("\\","/")):
-        App(root).load(from_file.replace("\\","/"))#Create a window with the selected client open
+def create_window(root, from_file="file which will not exist on anybody's file path"):
+    if os.path.exists(from_file.replace("\\", "/")):
+        App(root).load(from_file.replace("\\", "/"))  # Create a window with the selected client open
     else:
         App(root)
 
-#ROOT UTILITY FUNCTION
+
+# ROOT UTILITY FUNCTION
 def check_windows_open():
-    if len(root.winfo_children())==0:#Destroy root if no windows left open
+    if len(root.winfo_children()) == 0:  # Destroy root if no windows left open
         print("ROOT CLOSED AS NO WINDOWS DETECTED")
         root.destroy()
-    root.after(10000,check_windows_open)
+    root.after(10000, check_windows_open)
+
 
 # MAIN LOOP
 ############################################################################################################
 
 if __name__ == "__main__":
     splash_screen.destroy()
-    root=tk.CTk()
+    root = tk.CTk()
     root.withdraw()
-    #CHECK if file has been opened from a saved client
-    #sys.argv are the arguments such as opening a file!
-    if len(sys.argv)>1:
-        create_window(root,sys.argv[-1])
+    # CHECK if file has been opened from a saved client
+    # sys.argv are the arguments such as opening a file!
+    if len(sys.argv) > 1:
+        create_window(root, sys.argv[-1])
     else:
         create_window(root)
-    root.after(10000,check_windows_open)
-    #Run program mainloop
+    root.after(10000, check_windows_open)
+    # Run program mainloop
     root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
--- tests.py
+++ tests.py
@@ -2,43 +2,43 @@
 ###### PROGRAM WRITTEN BY - Adam Reynoldson reynoldson2002@gmail.com FOR Darren Reid via UpWork 2024
 ###### TESTS PDF FILES THROUGH THE CONVERTER PROGRAM
 
-from converter import *
 import os
+
+from converter import *
 
 LOCATION = r"C:\Users\reyno\Desktop\Freelancing\myDiscoveryResponses TESTS"
 
-FILES = [["Def SROGS to Sean (07944622xA9B4D)",37,"SPROG"],
-         ["TM RPD(3) to Plaintiff 1-29-24",21,"RFP"],
-         ["2022-03-30 KLOTZLE Def RFA",1,"RFA"],
-         ["2022-03-30 KLOTZLE Def RFP",15,"RFP"],
-         ["2022-03-30 KLOTZLE Def Special Interrogatories",29,"SPROG"],
-         ["2022-05-24 OWEN Def RFP1 [Quiroz] to Plf",14,"RFP"],
-         ["2022-05-24 OWEN Def SRogs1 [Quiroz] to Plf",18,"SPROG"],
-         ["2024-05-21 WILLIS-ALBRIGO Def DFP",19,"RFP"],
-         ["2024-05-21 WILLIS-ALBRIGO Def Srogs",17,"SPROG"],
-         ["BLAHA - Def City's RFP to PF",10,"RFP"],
-         ["BLAHA - Def City's SROGS to PF",15,"SPROG"],
-         ["City's RFP to PF, Set 1",17,"RFP"],
-         ["City's SPROGS to PF, Set 1",34,"SPROG"],
-         ["DISC RFA #1",21,"RFA"],
-         ["DISC RFP #1",20,"RFP"],
-         ["DISC SPROGS #1",35,"SPROG"],
-         ["REQ FOR ADM (Set One) v.1",10,"RFA"],
-         ["REQ for PRODUCTION (Set One) v.1 ",53,"RFP"],
-         ["RTP#1 TO LINDA",18,"RFP"],
-         ["SI#1 TO LINDA",39,"SPROG"],
-         ["SROGS Set one",34,"SPROG"]]
-
+FILES = [["Def SROGS to Sean (07944622xA9B4D)", 37, "SPROG"],
+         ["TM RPD(3) to Plaintiff 1-29-24", 21, "RFP"],
+         ["2022-03-30 KLOTZLE Def RFA", 1, "RFA"],
+         ["2022-03-30 KLOTZLE Def RFP", 15, "RFP"],
+         ["2022-03-30 KLOTZLE Def Special Interrogatories", 29, "SPROG"],
+         ["2022-05-24 OWEN Def RFP1 [Quiroz] to Plf", 14, "RFP"],
+         ["2022-05-24 OWEN Def SRogs1 [Quiroz] to Plf", 18, "SPROG"],
+         ["2024-05-21 WILLIS-ALBRIGO Def DFP", 19, "RFP"],
+         ["2024-05-21 WILLIS-ALBRIGO Def Srogs", 17, "SPROG"],
+         ["BLAHA - Def City's RFP to PF", 10, "RFP"],
+         ["BLAHA - Def City's SROGS to PF", 15, "SPROG"],
+         ["City's RFP to PF, Set 1", 17, "RFP"],
+         ["City's SPROGS to PF, Set 1", 34, "SPROG"],
+         ["DISC RFA #1", 21, "RFA"],
+         ["DISC RFP #1", 20, "RFP"],
+         ["DISC SPROGS #1", 35, "SPROG"],
+         ["REQ FOR ADM (Set One) v.1", 10, "RFA"],
+         ["REQ for PRODUCTION (Set One) v.1 ", 53, "RFP"],
+         ["RTP#1 TO LINDA", 18, "RFP"],
+         ["SI#1 TO LINDA", 39, "SPROG"],
+         ["SROGS Set one", 34, "SPROG"]]
 
 passed = 0
 for f in FILES:
-    filename = os.path.join(LOCATION,f[0])+".pdf"
-    reqs,req_type,doc_details,custom_keys = getRequests(filename)
-    if len(reqs)==f[1] and req_type==f[2]:#TEST PASSED
-        print("✓ "+str(f[0]))
-        passed+=1
-    else:#TEST FAILED
-        print("❌ FAILED: "+str(f)+"\n"+str(len(reqs))+"  "+str(req_type))
+    filename = os.path.join(LOCATION, f[0]) + ".pdf"
+    reqs, req_type, doc_details, custom_keys = getRequests(filename)
+    if len(reqs) == f[1] and req_type == f[2]:  # TEST PASSED
+        print("✓ " + str(f[0]))
+        passed += 1
+    else:  # TEST FAILED
+        print("❌ FAILED: " + str(f) + "\n" + str(len(reqs)) + "  " + str(req_type))
 
 print("----------------------------\n")
-print(str(passed)+"/"+str(len(FILES))+" Tests Passed!")
+print(str(passed) + "/" + str(len(FILES)) + " Tests Passed!")
